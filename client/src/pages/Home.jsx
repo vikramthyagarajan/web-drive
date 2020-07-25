873efd37917ce6c0f00c18b5c3a0d0dd98e98909
Vikram Thyagarajan <vikramthyagarajan92@gmail.com>
2020-07-25 15:38:48 +0530

    added dispatch to useEffect hook

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,21 +2,23 @@ import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 
 import { callGetUserAndFolder } from '../state/folder/folder.controller';
+import Sidebar from '../components/Sidebar/Sidebar';
+import MainView from '../components/MainView/MainView';
 import './Home.styles.scss';
 
 export default function() {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(callGetUserAndFolder());
-  }, [])
+  }, [dispatch])
 
   return (
     <div className="page homepage">
       <div className="sidebar-wrapper">
-        Sidebar
+        <Sidebar />
       </div>
       <div className="main-wrapper">
-        Main
+        <MainView />
       </div>
     </div>
   )
