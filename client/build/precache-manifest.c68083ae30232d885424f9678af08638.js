2782bfe9f76c509c58e33ce5472d270f29998001
Vikram Thyagarajan <vikramthyagarajan92@gmail.com>
2020-07-26 17:20:55 +0530

    Adding build files to git so its easier to run web client

---
+++ client/build/precache-manifest.c68083ae30232d885424f9678af08638.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53cc4432ea1d5d17270cc83b9afb130f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0427945d6691906d273",
+    "url": "/static/css/main.2812337d.chunk.css"
+  },
+  {
+    "revision": "eb61ba2c6c18730151be",
+    "url": "/static/js/2.d32634e4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.d32634e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0427945d6691906d273",
+    "url": "/static/js/main.f30b00ea.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
